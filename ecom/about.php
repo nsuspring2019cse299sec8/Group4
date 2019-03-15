2689c7ab20efcf2bca4b4ab455e2928308a6ccf1
prottasha07 <47690492+prottasha07@users.noreply.github.com>
2019-03-15 21:29:40 +0600

    Add files via upload
    "About.php" file uploaded. in this page there will be the information of the webpage creators and also some necessary infos.,

---
+++ ecom/about.php
@@ -0,0 +1,60 @@
+<?php require_once("../resources/config.php"); ?>
+<?php include(TEMPLATE_FRONT . DS . "header.php") ?>
+<?php include(TEMPLATE_FRONT . DS . "top_nav.php") ?>
+
+
+
+
+
+
+         <!-- Contact Section -->
+
+<div class="container">
+
+
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">About Us</h2>
+                <h3 class="section-subheading "> <?php display_message(); ?> </h3>
+            </div>
+        </div>
+
+        <div class="jumbotron main-jumbotron">
+          <div class="container">
+            <div class="content">
+              <h1>Surprise Celebration</h1>
+              <p class="margin-bottom">An Online Cakeshop </p>
+
+            </div>
+          </div>
+        </div>
+
+
+
+      <div class="row">
+
+        <section>
+          <div class="container">
+            <h2> Welcome to the Surprise Celebration.</h2>
+            <br>
+            <h3>Prottasha</h3>
+            <p class="lead">Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.</p>
+            <br>
+            <h3>Akib</h3>
+            <p class="lead">Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.</p>
+            <br>
+            <h3>Sakib</h3>
+            <p class="lead">Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.</p>
+
+
+          </div>
+        </section>
+
+      </div> <!-- /.row -->
+
+
+
+  </div> <!-- /.container -->
+
+
+<?php include(TEMPLATE_FRONT . DS . "footer.php") ?>
