e5b9c57cf4bb32ae5f80a112144fba3879e1adfb
prottasha07 <47690492+prottasha07@users.noreply.github.com>
2019-03-29 22:44:40 +0600

    Email config
    this is the config file of email. that has been used in php mailer

---
+++ ecom/public/classes/EmailConfigClass.php
@@ -0,0 +1,39 @@
+<?php
+
+class LoginClass {
+
+  const SMTP_HOST = 'smtp.gmail.com';
+
+  const SMTP_USER = 'surprisecelebration2019@gmail.com'; // //abimahmood16@gmail.com
+
+  const SMTP_PASSWORD = 'test5226';
+
+  const SMTP_PORT = 465;
+
+  const MAIL_FROM = "CakeShop";
+
+
+}
+
+
+
+class ContactUs {
+
+  const SMTP_HOST = 'smtp.gmail.com';              // Specify main and backup SMTP servers
+
+  const SMTP_USER = 'surprisecelebration2019@gmail.com';      // SMTP username
+
+  const SMTP_PASSWORD = 'test5226';                // SMTP password
+
+  const SMTP_PORT = 465;                           //Gmail SMTP port, TCP port to connect to
+
+  const MAIL_TO = 'abimahmood.bd@gmail.com';       // Add a recipient who will get the Email
+
+
+
+
+}
+
+
+
+ ?>
